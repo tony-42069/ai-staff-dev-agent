fa8b77077e40c9694c2ff4219031739a589809db
tony-42069 <dsadellari@abizadvisors.com>
2025-01-12 00:37:27 -0500

    fix: update MSW and testing-library setup with correct versions and imports

--- dashboard/frontend/src/setupTests.ts
+++ dashboard/frontend/src/setupTests.ts
@@ -1,6 +1,10 @@
 import '@testing-library/jest-dom';
-import { server } from './mocks/server';
+import { setupServer } from 'msw/node';
+import { handlers } from './mocks/handlers';
 
-beforeAll(() => server.listen());
+// Setup requests interception using the given handlers
+export const server = setupServer(...handlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close()); 
